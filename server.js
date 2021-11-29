9502ca1baff77b1d13c138a7a78f6650f5144810
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-28 23:53:29 -0500

    corregir

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/tienda-angular'));
+app.use(express.static(__dirname + '/dist/TiendaAngular'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/tienda-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/TiendaAngular/index.html'));
 });
 
 // default Heroku port
